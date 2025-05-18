22e80cb6065334f42dafb2fb42fc66708bdfef9e
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-05-18 15:53:31 -0500

    added features to the header

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { features } from "../constants";
+import Button from "../components/Button";
+import { Element } from "react-scroll";
+import { details, features } from "../constants/index";
 
 const Features = () => {
   return (
@@ -8,8 +9,49 @@ const Features = () => {
         <div className="container">
           <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g/ max-md:border-none max-md:rounded-none max-md:gap-3">
             {features.map(({ id, icon, caption, title, text, button }) => (
-              <div className="relative z-2 md:px-10 px-"></div>
+              <div
+                key={id}
+                className="relative z-2 md:px-10 px-5 md:pb-10 pb-5 flex-50 max-md:g7 max-md:border-2 max-md:border-s3 max-md:roundded-3xl max-md:flex-320"
+              >
+                <div className="w-full flex justify-start items-start">
+                  <div className="-ml-mb-12 flex items-center justify-center flex-col">
+                    <div className="w-0.5 h-16 bg-s3">
+                      <img
+                        src={icon}
+                        className="size-28 object-contain"
+                        alt={title}
+                      />
+                    </div>
+                  </div>
+                  <p className="caption mb-5 max-md:mb-6">{caption}</p>
+                  <h2 className="max-w-400 mb-7 h3 text-p4 max-md:mb-6 max-md:h5">
+                    {title}
+                  </h2>
+                  <p className="mb-11 body-1 max-md:mb-8 max-md:body-3">
+                    {text}
+                  </p>
+                  <Button icon={button.icon}>{button.title}</Button>
+                </div>
+              </div>
             ))}
+            <ul className="relative flex justify-around flex-grow px-[5%] border-2 bordedr-s3 rounded-7xl max-md:hidden">
+              <div className="absolute bg-s3/20 top-[38%] left-0 right-0 w-full h-[1px] z-10" />
+              {details.map(({ id, icon, title }) => (
+                <li key={id} className="relatie pt-16 px-4 pb-14">
+                  <div className="absolute top-8 bottom-0 left-1/2 bg-s3/20 w-[1px]h-full z-10" />
+                  <div className="flex items-center justify-center mx-auto mb-3 border-2 border-s2 rounded-full hover:border-s4 transition-all duration-500 shadow-500 size-20">
+                    <img
+                      src={icon}
+                      className="size-17/20 object-contain z-20"
+                      alt={title}
+                    />
+                  </div>
+                  <h3 className="relative z-2 max-w-36 mx-auto my-0 base-small text-center uppercase">
+                    {title}
+                  </h3>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </Element>
