a0b3f62e30449897a828942f192bd06c94de473c
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-05-21 12:42:47 -0500

    working on faq

---
+++ src/sections/Faq.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Faq = () => {
+  return (
+    <section>
+      <Element name="faq" className="relative">
+        <div className="container relative z-2 py-28">
+          <div>
+            <h3 className="h3 max-md:h5 max-w-640 max-lg:max-w-md mb-7 text-p4"></h3>
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Faq;
