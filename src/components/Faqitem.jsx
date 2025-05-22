be8c273c46209061cd314304f357d080606fd73e
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-05-22 13:57:12 -0500

    working on the questions in faq

---
+++ src/components/Faqitem.jsx
@@ -0,0 +1,32 @@
+import clsx from "clsx";
+import React, { useState } from "react";
+
+const Faqitem = ({ item }) => {
+  const { activeId, setActiveId } = useState(null);
+  const active = activeId === item.id;
+  return (
+    <div className="relative z-2 mb-16">
+      <div
+        className="group relative flex cursor-pointer items-center justify-between gap-10 px-7"
+        onClick={() => {}}
+      >
+        <div className="flex-1">
+          <div className="small-compact mb-1.5 text-p3 max-lg:hidden">
+            {index < 10 ? "0" : ""}
+            {index}
+          </div>
+          <div
+            className={clsx(
+              "h6 text-p4 transition-colors duraction-500 max-md:flex max-md:min-h-20 max-md:items-center",
+              active && "max-md:text-p1"
+            )}
+          >
+            {item.question}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Faqitem;
