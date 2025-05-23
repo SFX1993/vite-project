06ed7375a6a42143860485e3ba42e435759c8672
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-05-23 12:23:38 -0500

    working on the faqitems

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -26,7 +26,13 @@ const Faq = () => {
                 <Faqitem key={item.id} item={item} index={index} />
               ))}
             </div>
+            <div className="relative flex-1 lg:pt-24">
+              {Faq.slice(halfLength).map((item, index) => (
+                <Faqitem key={item.id} item={item} index={halfLength + index} />
+              ))}
+            </div>
           </div>
+          <div className="faq-lin_after absolute left-[calc(50%-1px)] top-0 -z-1 h-full w-0.5 bg-s2 max-lg:hidden"></div>
         </div>
       </Element>
     </section>
