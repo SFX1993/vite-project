3a4bf8ded1cd3dd6d6b6c935c9af3ba1934dbea3
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-05-25 21:32:59 -0500

    working on faq answer section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
 import Faq from "./sections/Faq";
+import Testimonials from "./sections/Testimonials";
+import Download from "./sections/Download";
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
       <Features />
       <Pricing />
       <Faq />
+      <Testimonials />
+      <Download />
     </main>
   );
 };
